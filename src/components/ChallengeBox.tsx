bf5de0c8df2db320aa34df2918535c2e2b4364f0
LuanaDG <luanadelagiustina@gmail.com>
2021-02-25 14:32:32 -0300

    finalizando Countdown Context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,23 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
 
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+    const { resetCountdown } = useContext(CountdownContext);
+
+    function handleChallengeSucceeded() {
+        completeChallenge();
+        resetCountdown();
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge();
+        resetCountdown();
+    }
+
 
 
     return (
@@ -21,11 +34,14 @@ export function ChallengeBox() {
                     <footer>
                         <button type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
-                        >Falhei</button>
+                            onClick={handleChallengeFailed}
+                        >Falhei
+                        </button>
                         <button type="button"
                             className={styles.challengeSucceededButton}
-                        >Completei</button>
+                            onClick={handleChallengeSucceeded}
+                        >Completei
+                        </button>
                     </footer>
                 </div>
 
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,49 +1,24 @@
 import { useState, useEffect, useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css'
 
-let countdownTimeout: NodeJS.Timeout;
+
 
 export function Countdown() {
 
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-    const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+    const { minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
+    } = useContext(CountdownContext)
 
     /* se não houver dois caracteres o padStart vai preencher com 0 o espaço da esquerda */
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
-    function startCountdown() {
-        setIsActive(true);
-    }
-
-    function resetCountdown() {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.1 * 60);
-    }
-
-    /*  recebe dois parâmetros: 1 {} uma função - o q eu quero executar?
-     2 [] quando eu quero executar? sempre q algum valor mudar, neste caso o active 
-     toda vez q o valor do seguno parâmetro mudar vai executar a primira função*/
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1)
-            }, 1000);
-        } else if (isActive && time === 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time])
 
     return (
         <div>
